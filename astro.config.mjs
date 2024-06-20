bebfcf5e9e3648d9f56f2d56f12ff4ff53735aa8
en-vang <enrique@si.team>
2024-06-19 22:05:54 -0400

    Github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+const BASE_URO = "https://github.com/vang609/portfolio.dev";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  site: "https://vang609.github.io",
+  base: "portfolio.dev",
+  integrations: [tailwind()],
 });
